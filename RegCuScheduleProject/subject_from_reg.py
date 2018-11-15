e87cbbb2d167a97d14651de030db67fed23dce8f
suppapat_n <suppapat_n@yahoo.com>
2018-11-16 02:27:22 +0700

    class Semester

--- RegCuScheduleProject/subject_from_reg.py
+++ RegCuScheduleProject/subject_from_reg.py
@@ -1,5 +1,6 @@
-from django.contrib.sites import requests
 
+import requests
+from bs4 import BeautifulSoup
 
 class Semester():
 
@@ -14,6 +15,48 @@ class Semester():
         self.academicYear=academicYear
 
     def searchCourseList(self,courseNo,courseName,courseType):
+        ##calculate course type to fill gened code
+        param={ 'examdateCombo':'I2017207/05/1475',
+                'studyProgram':self.studyProgram, #S
+                'semester':self.semester, #1/2/3
+                'acadyearEfd':self.academicYear,
+                'submit.x':'0',
+                'submit.y':'0',
+                'courseno':courseNo,
+                'coursename':courseName,
+                'examdate':'',
+                'examstartshow':'',
+                'examendshow':'',
+                'faculty':'',
+                'coursetype':'',
+                'genedcode':'',
+                'cursemester':'1',
+                'curacadyear':self.academicYear,
+                'examstart':'',
+                'examend':'',
+                'activestatus':'OFF',
+                'acadyear':self.academicYear,
+                'lang':'T',
+                'download':'download'}
         url_head = 'https://cas.reg.chula.ac.th/servlet/com.dtm.chula.cs.servlet.QueryCourseScheduleNew.QueryCourseScheduleNewServlet'
-        cook = requests.get(url_head, verify=False)
-        self.cookie = cook.cookies
+        head = requests.get(url_head, verify=False)
+        self.cookie = head.cookies
+        url_left = "https://cas.reg.chula.ac.th/servlet/com.dtm.chula.cs.servlet.QueryCourseScheduleNew.CourseListNewServlet"
+        result_left = requests.get(url_left, verify=False, cookies=self.cookie,params=param)
+        result_left.encoding = 'utf-8'
+        soup_all = BeautifulSoup(result_left.content, "html.parser")
+        data_all=soup_all("tr")
+        course_list=[]
+        for data in data_all:
+            soup_sub = BeautifulSoup(str(data), "html.parser")
+            courseID=soup_sub.find("font",{"face":"MS Sans Serif",'size':'-1'})
+            courseName = soup_sub.find("font", {"face": "MS Sans Serif", 'size': '-2', 'color': "#660000"})
+            if courseID and courseName is not None:
+                courseID=courseID.get_text()
+                courseName=courseName.get_text()
+            if courseID is not None and len(courseID)>2 and courseName is not None:
+                course_list.append((courseID,courseName[2:]))
+        #data=soup.find_all("font",{"face":"MS Sans Serif",'color':'#660000'})"""
+        return course_list
+
+
